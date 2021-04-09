0f3c2ea016c1b26326d2c437837e4cf521724423
RyuBAI <me@ryubai.com>
2021-04-09 18:35:05 +0800

    Create prime.h

---
+++ src/prime.h
@@ -0,0 +1,27 @@
+
+constexpr std::size_t GEN_MAX = 1000;
+
+//Finding primes with Sieve of Eratosthenes algorithm
+constexpr auto PRIME_GEN() -> std::array<bool, GEN_MAX>{
+    std::array<bool, GEN_MAX> rtn = { };
+    rtn[0] = false; rtn[1] = false; rtn[2] = true; rtn[3] = true;
+    for(std::size_t i=4; i<GEN_MAX; i++) rtn[i] = true;
+    for(std::size_t i=2; i<GEN_MAX; i++){
+        std::size_t j = i + i;
+        while(j<GEN_MAX){ rtn[j] = false; j += i; }
+    }
+    return rtn;
+}
+std::array<bool, GEN_MAX> PRIME_LIST = PRIME_GEN();
+
+
+void printSmallerPrimes(int from, int to){
+    if((to >= GEN_MAX) || (from < 0) || (from > to)) return;
+    for(int i=from; i<to; i++) if(PRIME_LIST[i]) std::cout << i << " ";
+    std::cout << std::endl;
+}
+
+static int soe_test = push_test("Sieve of Eratosthenes", (test_function)[](){
+    printSmallerPrimes(900, 999);
+    return 0;
+});
